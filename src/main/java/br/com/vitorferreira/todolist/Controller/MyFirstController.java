9832cbf0e26a4b10b12e2d1db234366d989b7580
techvitor <vitorftn4@gmail.com>
2026-02-14 10:53:20 -0300

    First Controller

--- src/main/java/br/com/vitorferreira/todolist/Controller/MyFirstController.java
+++ src/main/java/br/com/vitorferreira/todolist/Controller/MyFirstController.java
@@ -1,6 +1,27 @@
 package br.com.vitorferreira.todolist.Controller;
 
+// Componente que vai atuar entre as camadas
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/primeiraRota")
+// http://localhost:8080/
 public class MyFirstController {
 
+    /*
+    * Metodo de acessos HTTP
+    * GET - Buscar uma informação)
+    * PUSH - Adicionar dado ou informação)
+    * PUT - Alterar um dado/informação
+    * DELETE - Remover um dado
+    * PATCH - Alterar somente uma parte da info/dado
+     */
 
+    @GetMapping("/")
+    public String primeiraMensagem(){
+        return "Primeira mensagem";
+    }
 }
---
+++ src/main/java/br/com/vitorferreira/todolist/User/UserController.java
@@ -0,0 +1,4 @@
+package br.com.vitorferreira.todolist.User;
+
+public class UserController {
+}
