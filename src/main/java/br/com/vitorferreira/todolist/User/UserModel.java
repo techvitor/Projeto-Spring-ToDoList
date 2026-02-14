95b9054e8f50d8cf8db6099c825496009b88d962
techvitor <vitorftn4@gmail.com>
2026-02-14 11:18:59 -0300

    Lombok

---
+++ src/main/java/br/com/vitorferreira/todolist/User/UserModel.java
@@ -0,0 +1,16 @@
+package br.com.vitorferreira.todolist.User;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+
+public class UserModel {
+
+    private String username;
+    private String password;
+    private String email;
+
+
+}
